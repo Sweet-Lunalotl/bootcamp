dfc67cc8c3c7075cc471bbda12a0b68b613f04b9
Brüntrup <lunanoxfea.bruentrup@swb-gruppe.de>
2025-10-14 13:07:42 +0200

    Finished task number pyramid

--- Aufgaben/typescript/src/numberPyramid.ts
+++ Aufgaben/typescript/src/numberPyramid.ts
@@ -1,55 +1,42 @@
-
-//This would work if undefiend === 0
+/**
+ * Creates and returns a number pyramid in a 3-dimensional array
+ *
+ * @param levels - height of the number pyramid
+ * @returns 3-dimensional array filled with a number pyramid
+ */
 function buildPyramid(levels: number): number[][][]{
-  const pyramid: number [][][] = [[[1]],[[1,1], [1,1]]];
-  for(let level: number = 1; level < levels; level++){
-    pyramid.push([[1]]);
-    //[[1]],[[1,1], [1,1]], [1]
-    for(let innerLayers: number = 0; innerLayers < pyramid[level-1].length+1; innerLayers++){
-      for(let stone: number = 1; stone < pyramid[level-1][0].length; stone++){
+  const pyramid: number[][][] = [[[1]]];
+  for(let seam: number = 1; seam <= levels; seam++){
+    pyramid.push([]);
+    for(let innerLayers: number = 0; innerLayers <= seam; innerLayers++){
+      pyramid[seam].push([]);
+      for(let stone: number = 0; stone <= seam; stone++){
         let addNumber: number = 0;
-        try{
-          addNumber = addNumber + pyramid[level-1][innerLayers-1][stone-1];
-        }
-        catch (error){}
-        try{
-          addNumber = addNumber + pyramid[level-1][innerLayers-1][stone];
-        }
-        catch (error){}
-        try{
-          addNumber = addNumber + pyramid[level-1][innerLayers][stone-1];
-        }
-        catch (error){}
-        try{
-          addNumber = addNumber + pyramid[level-1][innerLayers][stone];
-        }
-        catch (error){}
-
-
-
-
-        //pyramid[level][innerLayers].push(pyramid[level-1][innerLayers-1][stone-1] + pyramid[level-1][innerLayers-1][stone] + pyramid[level-1][innerLayers][stone-1] + pyramid[level-1][innerLayers][stone]);
+        addNumber = addNumber + (pyramid[seam-1]?.[innerLayers-1]?.[stone-1] ?? 0);
+        addNumber = addNumber + (pyramid[seam-1]?.[innerLayers-1]?.[stone] ?? 0);
+        addNumber = addNumber + (pyramid[seam-1]?.[innerLayers]?.[stone-1] ?? 0);
+        addNumber = addNumber + (pyramid[seam-1]?.[innerLayers]?.[stone] ?? 0);
+        pyramid[seam][innerLayers].push(addNumber);
       }
     }
   }
   return pyramid;
 }
 
-
-const pyramides: number [][][] = [[[1]],[[1,1], [1,1]]];
-const levels: number = 3;
-
-console.log(buildPyramid(levels))
-
-/*
-
-pyramides[1][1].push(2)
-pyramides[1].push([3])
-pyramides.push([[3]])
-console.log(pyramides)
-console.log(pyramides[1])
-console.log(pyramides[1][2].length)
-console.log(pyramides[1].length)
-
-
+/**
+ * Returns a Layer of the number pyramid
+ *
+ * @param layer - The layer of the number Pyramid that should be returned
+ * @param pyramidArray - 3-dimensional array containing a number pyramid made with buildPyramid()
+ * @returns A Layer of the number pyramid
  */
+function pyramidLayer(layer: number, pyramidArray: number[][][]): number[][]{
+  return pyramidArray[layer-=1];
+}
+
+
+
+
+const pyramidHeight: number = 4;
+const numberPyramid: number[][][] = buildPyramid(pyramidHeight);
+console.log(pyramidLayer(pyramidHeight, numberPyramid));
