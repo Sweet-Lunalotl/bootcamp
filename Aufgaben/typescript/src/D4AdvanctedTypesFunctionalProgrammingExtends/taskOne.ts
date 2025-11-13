ce6958ee60e850afbeb280eec2c138acebdbad07
Brüntrup <lunanoxfea.bruentrup@swb-gruppe.de>
2025-11-13 12:20:08 +0100

    more thoughts

--- Aufgaben/typescript/src/D4AdvanctedTypesFunctionalProgrammingExtends/taskOne.ts
+++ Aufgaben/typescript/src/D4AdvanctedTypesFunctionalProgrammingExtends/taskOne.ts
@@ -13,3 +13,5 @@ const exampleTwo: nonEmpty<Number> = [1, 2];
 //task 1b
 //What should the array be filled with?! Or should it just accept non empty arrays? I think the second one must be right
 //What even is an object type containing arrays? For Example an interface containing multiple arrays as their parameters
+//but it should not be an interface it should be a type AHHHHHH
+//and how should I change something without actually calling a function?
